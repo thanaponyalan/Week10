7055e54f5e7495b49d18faf9192a1c239416c99e
thanaponyalan <thanaponyalan@gmail.com>
2018-12-17 06:50:22 +0700

    Button

---
+++ Lab10_ControlStudy/Lab10_ControlStudy/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10_ControlStudy
+{
+    public partial class Form1 : Form
+    {
+        Button btnHello;
+        public Form1()
+        {
+            InitializeComponent();
+            btnHello = new Button();
+            btnHello.Location = new Point(20,20);
+            btnHello.Text = "Hello";
+            this.Controls.Add(btnHello);
+        }
+    }
+}
