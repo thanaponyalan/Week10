57db411d46e370524df348f341354bba9a11d46e
thanaponyalan <thanaponyalan@gmail.com>
2018-12-17 16:19:44 +0700

    FolderBrowserDialog

---
+++ folderBrowser/folderBrowser/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace folderBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFolderBrowser = new System.Windows.Forms.Button();
+            this.txtboxFolder = new System.Windows.Forms.TextBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFolderBrowser
+            // 
+            this.btnFolderBrowser.Location = new System.Drawing.Point(29, 12);
+            this.btnFolderBrowser.Name = "btnFolderBrowser";
+            this.btnFolderBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnFolderBrowser.TabIndex = 0;
+            this.btnFolderBrowser.Text = "Browse";
+            this.btnFolderBrowser.UseVisualStyleBackColor = true;
+            this.btnFolderBrowser.Click += new System.EventHandler(this.btnFolderBrowser_Click);
+            // 
+            // txtboxFolder
+            // 
+            this.txtboxFolder.Location = new System.Drawing.Point(29, 54);
+            this.txtboxFolder.Name = "txtboxFolder";
+            this.txtboxFolder.Size = new System.Drawing.Size(100, 20);
+            this.txtboxFolder.TabIndex = 1;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(221, 12);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.SelectedObject = this.folderBrowserDialog1;
+            this.propertyGrid1.Size = new System.Drawing.Size(184, 236);
+            this.propertyGrid1.TabIndex = 2;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 288);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.txtboxFolder);
+            this.Controls.Add(this.btnFolderBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFolderBrowser;
+        private System.Windows.Forms.TextBox txtboxFolder;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ folderBrowser/folderBrowser/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace folderBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFolderBrowser_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                propertyGrid1.Refresh();
+                txtboxFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
