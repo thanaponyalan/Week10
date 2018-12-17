cbe15daa07970d8cc810f941be713288a1a882fe
thanaponyalan <thanaponyalan@gmail.com>
2018-12-17 16:53:43 +0700

    fontDialog

---
+++ fontDialog/fontDialog/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace fontDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFontDialog = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.btnUpdateRichTextBox1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFontDialog
+            // 
+            this.btnFontDialog.Location = new System.Drawing.Point(12, 12);
+            this.btnFontDialog.Name = "btnFontDialog";
+            this.btnFontDialog.Size = new System.Drawing.Size(128, 33);
+            this.btnFontDialog.TabIndex = 0;
+            this.btnFontDialog.Text = "Font Dialog";
+            this.btnFontDialog.UseVisualStyleBackColor = true;
+            this.btnFontDialog.Click += new System.EventHandler(this.btnFontDialog_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 150);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(321, 65);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(435, 12);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.SelectedObject = this.fontDialog1;
+            this.propertyGrid1.Size = new System.Drawing.Size(353, 426);
+            this.propertyGrid1.TabIndex = 3;
+            // 
+            // btnUpdateRichTextBox1
+            // 
+            this.btnUpdateRichTextBox1.Location = new System.Drawing.Point(12, 111);
+            this.btnUpdateRichTextBox1.Name = "btnUpdateRichTextBox1";
+            this.btnUpdateRichTextBox1.Size = new System.Drawing.Size(128, 33);
+            this.btnUpdateRichTextBox1.TabIndex = 4;
+            this.btnUpdateRichTextBox1.Text = "Update Text";
+            this.btnUpdateRichTextBox1.UseVisualStyleBackColor = true;
+            this.btnUpdateRichTextBox1.Click += new System.EventHandler(this.btnUpdateRichTextBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdateRichTextBox1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnFontDialog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFontDialog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button btnUpdateRichTextBox1;
+    }
+}
+
---
+++ fontDialog/fontDialog/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fontDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFontDialog_Click(object sender, EventArgs e)
+        {
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Font = fontDialog1.Font;
+                propertyGrid1.Refresh();
+            }
+        }
+
+        private void btnUpdateRichTextBox1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = textBox1.Text;
+        }
+    }
+}
